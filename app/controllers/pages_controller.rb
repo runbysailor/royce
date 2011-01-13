164d7d05e0896d123a5f0be2e1470989eb5f7cf9
runbysailor <admin@runbysailor.com>
2011-01-12 23:07:17 -0600

    Initial commit with GoogleEarth Plugin example.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+class PagesController < ApplicationController
+
+	def home
+	end
+	
+	def about
+	end
+	
+	def research
+	end
+	
+	def portfolio
+	end
+	
+	def people
+	end
+	
+	def contact
+	end
+  
+  def ge
+    render :layout => false
+  end
+	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '40877cf4d002ec688a021e18c7c66db9483c8d03604114ca90088ba5feb7a4933f376091a774971bf8351f03c43c38c397761336b891dfde3c3114988fd96a2f'
